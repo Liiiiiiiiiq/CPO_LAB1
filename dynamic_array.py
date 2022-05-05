66dfd784a697f11a2c0f695b6558d2884f7bc760
wangzimeng <1372178297@qq.com>
2022-05-05 22:44:39 +0800

    Add type hints

--- dynamic_array.py
+++ dynamic_array.py
@@ -1,11 +1,32 @@
+from typing import Callable
+from typing import Optional
+
+
+class DynamicArrayIterator(object):
+    def __init__(self, lst: list[Optional[int]], lng: int) -> None:
+        self.__chunk = lst
+        self.__length = lng
+        self.__start_num = -1
+
+    def __iter__(self) -> 'DynamicArrayIterator':
+        return self
+
+    def __next__(self) -> Optional[int]:
+        # wzm
+        self.__start_num += 1
+        if self.__start_num >= self.__length:
+            raise StopIteration()
+        return self.__chunk[self.__start_num]
+
+
 class DynamicArray(object):
-    def __init__(self, capacity=10, grow_factor=1.2):
+    def __init__(self, capacity: int = 10, grow_factor: float = 1.2):
         self.__grow_factor = grow_factor
         self.__length = 0
         self.__capacity = capacity  # Initialize chunk size to 10
-        self.__chunk = [None] * self.__capacity
+        self.__chunk: list[Optional[int]] = [None] * self.__capacity
 
-    def add(self, element):
+    def add(self, element: Optional[int]) -> None:
         # llq
         if element is not None and type(element) != int:
             raise Exception('Input data must be int or None')
@@ -14,10 +35,13 @@ class DynamicArray(object):
                              - self.__capacity
             self.__chunk += [None] * new_chunk_size
             self.__capacity = self.__capacity + new_chunk_size
-        self.__chunk[self.__length] = element
+        if element is None:
+            pass
+        else:
+            self.__chunk[self.__length] = element
         self.__length += 1
 
-    def set(self, pos, value):
+    def set(self, pos: int, value: Optional[int]) -> None:
         # llq
         if value is not None and type(value) != int:
             raise Exception('Input data must be int or None')
@@ -25,7 +49,7 @@ class DynamicArray(object):
             raise Exception('The location accessed is not in the array!')
         self.__chunk[pos] = value
 
-    def remove(self, pos):
+    def remove(self, pos: int) -> None:
         # llq
         if pos < 0 or pos >= self.__length:
             raise Exception('The location accessed is not in the array!')
@@ -34,11 +58,11 @@ class DynamicArray(object):
             self.__chunk[pos] = self.__chunk[pos + 1]
             pos += 1
 
-    def size(self):
+    def size(self) -> int:
         # llq
         return self.__length
 
-    def member(self, value):
+    def member(self, value: int) -> bool:
         # llq
         if value is not None and type(value) != int:
             raise Exception('Input data must be int or None')
@@ -47,7 +71,7 @@ class DynamicArray(object):
                 return True
         return False
 
-    def reverse(self):
+    def reverse(self) -> None:
         # llq
         left = 0
         right = self.__length - 1
@@ -58,7 +82,7 @@ class DynamicArray(object):
             right -= 1
             left += 1
 
-    def from_list(self, lst):
+    def from_list(self, lst: list[Optional[int]]) -> None:
         # llq
         if lst is None:
             raise Exception('The input array is empty!')
@@ -69,93 +93,58 @@ class DynamicArray(object):
         self.__capacity = self.__length + 10
         self.__chunk = lst + [None] * 10
 
-    def to_list(self):
+    def to_list(self) -> list[Optional[int]]:
         # llq
         return [self.__chunk[i] for i in range(self.__length)]
 
-    def filter(self, predicate):
+    def filter(self, predicate: Callable[[Optional[int]], bool]) -> None:
         # wzm
-        result = []
-        for i in range(self.__length):
-            if predicate(self.__chunk[i]):
-                result.append(self.__chunk[i])
-            else:
-                i += 1
-        return result
+        for i in range(self.__length-1, -1, -1):
+            if not predicate(self.__chunk[i]):
+                self.remove(i)
 
-    def map(self, function, *iterables):
+    def map(self, function: Callable[..., int], *iters: tuple['DynamicArray', ...]) -> None:
         # wzm
         if self.__length == 0:
-            return
-        i = 0
-        min_len = 1e10
-        for it in iterables:
-            if min_len > len(it):
-                min_len = len(it)
-        length = self.__length
-        if length > min_len:
-            for pos in range(length - 1, min_len - 1, -1):
-                self.remove(pos)
-        if len(iterables) > 0:
-            for args in zip(*iterables):
+            pass
+        if len(iters) > 0:
+            i = 0
+            for args in zip(*iters):
                 if i < self.__length:
-                    if self.__chunk[i] is None:
-                        pass
-                    else:
-                        if None in args:
-                            self.__chunk[i] = None
-                        else:
-                            self.__chunk[i] = function(self.__chunk[i], *args)
+                    self.__chunk[i] = function(self.__chunk[i], *args)
                     i += 1
-                else:
-                    break
+            if i < self.__length:
+                for j in range(self.__length-1, i-1, -1):
+                    self.remove(j)
         else:
-            while i < self.__length:
-                if self.__chunk[i] is None:
-                    pass
-                else:
-                    self.__chunk[i] = function(self.__chunk[i])
-                i += 1
+            for i in range(self.__length):
+                self.__chunk[i] = function(self.__chunk[i])
 
-    def reduce(self, function, initial=None):
+    def reduce(self, function: Callable[[Optional[int], Optional[int]], int],
+               initial: Optional[int] = None) -> Optional[int]:
         # wzm
-        if self.__length == 0:
-            raise Exception("The array length cannot be 0!")
-        if self.__length == 1 and initial is None:
-            return self.__chunk[0]
+        it = iter(self)
         if initial is None:
-            value = 0
+            try:
+                value = next(it)
+            except StopIteration:
+                raise TypeError("reduce() of empty sequence with no "
+                                "initial value") from None
         else:
             value = initial
-        for i in range(self.__length):
-            value = function(value, self.__chunk[i])
+            
+        for element in it:
+            value = function(value, element)
         return value
 
-    def __iter__(self):
+    def __iter__(self) -> 'DynamicArrayIterator':
         # wzm
         return DynamicArrayIterator(self.__chunk, self.__length)
 
-    def __add__(self, other):
+    def __add__(self, other: 'DynamicArray') -> 'DynamicArray':
         # llq
         if type(other) != DynamicArray:
-            raise Exception('The type of connection is not DynamicArray!')
+            raise Exception('The type of concatenation is not DynamicArray!')
         for k in other:
             self.add(k)
         return self
-
-
-class DynamicArrayIterator(object):
-    def __init__(self, lst, lng):
-        self.__chunk = lst
-        self.__length = lng
-        self.__start_num = -1
-
-    def __iter__(self):
-        return self
-
-    def __next__(self):
-        # wzm
-        self.__start_num += 1
-        if self.__start_num >= self.__length:
-            raise StopIteration()
-        return self.__chunk[self.__start_num]
